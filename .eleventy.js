d672f96fd221d58cb28ac4c4bcc7ab2743fb869a
Henry Catalini Smith <henry@catalinismith.com>
2021-04-09 21:16:58 +0200

    picknmix

---
+++ .eleventy.js
@@ -0,0 +1,31 @@
+const fs = require("fs-extra")
+const htmlmin = require("html-minifier")
+const sass = require("sass")
+
+module.exports = function(eleventyConfig) {
+  console.log("picknmix")
+
+  eleventyConfig.addGlobalData(
+    "css",
+    function() {
+      return sass.renderSync({ file: "style.scss" }).css
+    }
+  )
+
+  eleventyConfig.addTransform(
+    "htmlmin",
+    function(content, outputPath) {
+      if (outputPath && outputPath.endsWith(".html")) {
+        content = htmlmin.minify(content, {
+          useShortDoctype: true,
+          removeComments: true,
+          collapseWhitespace: true
+        })
+      }
+      return content
+    }
+  )
+
+  eleventyConfig.addWatchTarget("style.scss")
+}
+
