aa4ba3ac777752c80ec438bdd28ab14fba6be429
Henry Catalini Smith <henry@catalinismith.com>
2021-04-11 12:35:24 +0200

    fix img url in production

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const fs = require("fs-extra")
 const htmlmin = require("html-minifier")
+const { JSDOM } = require("jsdom")
 const sass = require("sass")
 
 module.exports = function(eleventyConfig) {
@@ -23,6 +24,26 @@ module.exports = function(eleventyConfig) {
           removeComments: true,
           collapseWhitespace: true
         })
+      }
+      return content
+    }
+  )
+
+  const url = process.env.COMMIT_REF ? "https://hen.cat/picknmix" : ""
+
+  eleventyConfig.addTransform(
+    "links",
+    function(content, outputPath) {
+      if (outputPath && outputPath.endsWith(".html")) {
+        const dom = new JSDOM(content)
+        const images = dom.window.document.querySelectorAll("img")
+
+        for (const img of images) {
+          if (img.src.startsWith("/")) {
+            img.src = url + img.src
+          }
+        }
+        content = dom.serialize()
       }
       return content
     }
